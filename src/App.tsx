e194e7e0e6ce1bdadf0571ccee8da8338e482d1f
Dmitriyhk <dimassik50@gmail.com>
2022-03-09 19:43:32 +0300

    install react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Route, Router } from 'react-router-dom'
 import ItemLIst from './components/itemList/ItemLIst'
+import Navbar from './components/UI/navbar/Navbar'
 import './styles/App.css'
-
 const App = () => {
   return (
     <div>
+      <Navbar/>
       <ItemLIst/>
     </div>
   )
